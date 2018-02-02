2d01413f8b44dbc45817469eae9035b945621661
AlekseyPn <aleksfers@icloud.com>
2018-02-02 22:03:23 +0000

    args array handler and return result

--- movies.rb
+++ movies.rb
@@ -1,15 +1,28 @@
 # frozen_string_literal: true
 
-movie_name = ARGV.first
-def movies(name)
-  case name
-  when 'Titanic'
-    'Titanic is a good movie'
-  when 'Elki'
-    'Elki is a bad movie'
-  else
-    "Haven\'t seen #{name} yet"
+# movie_name = ARGV.first
+# def movies(name)
+#   case name
+#   when 'Titanic'
+#     'Titanic is a good movie'
+#   when 'Elki'
+#     'Elki is a bad movie'
+#   else
+#     "Haven\'t seen #{name} yet"
+#   end
+# end
+
+# puts movies movie_name
+movies_names = ARGV
+
+def favourite_movies(names)
+  names.each do |movie|
+    if ['Avengers', 'Galaxy guardians', 'Iron Man'].include?(movie)
+      puts "#{movie}. Oh it's my favourite film"
+    else
+      puts "#{movie}. What? No, it's not my favourite film!"
+    end
   end
 end
 
-puts movies movie_name
+puts favourite_movies movies_names
