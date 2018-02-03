b63b4b1e228e5873b9819a194b2c4ba6784255da
AlekseyPn <aleksfers@icloud.com>
2018-02-03 12:31:14 +0000

    add function for check description

--- movies.rb
+++ movies.rb
@@ -2,15 +2,19 @@
 
 movies_names = ARGV
 KNOWN_MOVIES = ['Avengers', 'Galaxy guardians', 'Iron Man'].freeze
+def check_movie_description(movie)
+  if movie == 'Elki'
+    'Elki is a bad movie'
+  elsif KNOWN_MOVIES.include?(movie)
+    "#{movie} is a good movie"
+  else
+    "Haven't seen #{movie} yet"
+  end
+end
+
 def favourite_movies(movies_names)
   movies_names.each do |movie|
-    if movie == 'Elki'
-      puts 'Elki is a bad movie'
-    elsif KNOWN_MOVIES.include?(movie)
-      puts "#{movie} is a good movie"
-    else
-      puts "Haven't seen #{movie} yet"
-    end
+    puts check_movie_description movie
   end
 end
 favourite_movies movies_names
