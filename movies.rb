803a3b5062e502638a72ca595355441cd0db7054
AlekseyPn <aleksfers@icloud.com>
2018-02-03 12:09:14 +0000

    fix

--- movies.rb
+++ movies.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 movies_names = ARGV
-FAVOURITE_MOVIES = ['Avengers', 'Galaxy guardians', 'Iron Man', 'Elki'].freeze
+KNOWN_MOVIES = ['Avengers', 'Galaxy guardians', 'Iron Man'].freeze
 def favourite_movies(movies_names)
   movies_names.each do |movie|
     if movie == 'Elki'
       puts 'Elki is a bad movie'
-    elsif FAVOURITE_MOVIES.include?(movie)
-      puts "#{movie}. #{favourite_movies_description[movie]}"
+    elsif KNOWN_MOVIES.include?(movie)
+      puts "#{movie} is a good movie"
     else
       puts "Haven't seen #{movie} yet"
     end
