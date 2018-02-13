fdafdeaa6d5a106e47256559c1c8fa39f6f129e5
AlekseyPn <aleksfers@icloud.com>
2018-02-13 00:34:56 +0000

    first version of task_03

--- movies.rb
+++ movies.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 @movies_file_path = ARGV.first || './movies.txt'
-MOVIES_DESCRIPTION_KEYS = %w[url title year_of_release country date genre timing rating producer actors].freeze
+MOVIES_DESCRIPTION_KEYS = %i[url title year country date genre timing rating producer actors].freeze
 RATING_START_POINT = 8.0
-FILTERED_WORD = 'Max'
+COUNTRY_FILTER_WORD = 'USA'
 def movies_list(path)
   File.read(path)
 end
@@ -25,20 +25,67 @@ def count_stars_via_rating(rating)
 end
 
 def update_rating_value(movie)
-  movie.update('rating' => movie['rating']) do |_key, _old, new|
+  movie.update(rating: movie[:rating]) do |_key, _old, new|
     count_stars_via_rating new
   end
 end
 
-def show_movies_of_file(file)
-  puts "Movies file: #{read_filename @movies_file_path}"
-  get_movies_description(file)
-    .map { |movie| description_for_movie movie }
-    .select { |movie| movie['title'].include?(FILTERED_WORD) }
-    .each do |movie|
-      update_rating_value movie
-      puts "Movie: #{movie['title']}]\nRating: #{movie['rating']}"
+def get_most_long_movies(movies)
+  movies.max_by(5) { |movie| movie[:timing].to_i }
+end
+
+def first_ten_comedies(movies)
+  movies.select { |movie| movie[:genre]['Comedy'] }
+        .max(10) do |a, b|
+          b[:year] <=> a[:year]
+        end
+end
+
+def producer_names(movies)
+  movies.map { |movie| movie[:producer] }
+        .sort_by { |producer| producer.split.last[0] }
+        .uniq
+end
+
+def quantity_not_american_movies(movies)
+  puts 'Quantity movies filmed not in the USA:'
+  puts movies.count { |movie| movie[:country] != COUNTRY_FILTER_WORD }
+end
+
+def get_movie_info(movie)
+  "#{movie[:title]}(#{movie[:date]}; #{movie[:genre].tr(',', '/')}) - #{movie[:timing]}"
+end
+
+def show_movies_info(movies)
+  movies.each do |movie|
+    puts get_movie_info movie
+  end
+end
+
+def show_most_long_movies(movies)
+  puts 'Five most long movies from top:'
+  show_movies_info get_most_long_movies movies
+end
+
+def show_first_ten_comedies(movies)
+  puts 'First ten comedies by year:'
+  show_movies_info first_ten_comedies movies
+end
+
+def show_producers(movies)
+  puts 'Producers filtered by alphabet:'
+  producer_names(movies)
+    .each do |producer|
+      puts producer
     end
+end
+
+def show_movies_of_file(movies)
+  movies = get_movies_description(movies)
+           .map { |movie| description_for_movie movie }
+  show_most_long_movies movies
+  show_first_ten_comedies movies
+  quantity_not_american_movies movies
 end
 
 show_movies_of_file movies_list @movies_file_path
